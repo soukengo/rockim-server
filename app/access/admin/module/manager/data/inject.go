b37b63a4ee784d73ff4146b7925ea26ee822c0e7
soukengo <soukengo@163.com>
2023-01-16 16:15:46 +0800

    修改目录

--- app/access/admin/module/manager/data/inject.go
+++ app/access/admin/module/manager/data/inject.go
@@ -2,10 +2,12 @@ package data
 
 import (
 	"github.com/google/wire"
+	"rockim/app/access/admin/module/manager/data/database"
 )
 
 // ProviderSet is data providers.
 var ProviderSet = wire.NewSet(
+	database.ProviderSet,
 	NewSysUserRepo,
 	NewSysRoleRepo,
 	NewSysResourceRepo,
--- app/access/admin/module/manager/inject.go
+++ app/access/admin/module/manager/inject.go
@@ -4,8 +4,7 @@ import (
 	"github.com/google/wire"
 	"rockim/app/access/admin/module/manager/biz"
 	"rockim/app/access/admin/module/manager/data"
-	"rockim/app/access/admin/module/manager/data/database"
 	"rockim/app/access/admin/module/manager/service"
 )
 
-var ProviderSet = wire.NewSet(database.ProviderSet, data.ProviderSet, biz.ProviderSet, service.ProviderSet)
+var ProviderSet = wire.NewSet(data.ProviderSet, biz.ProviderSet, service.ProviderSet)
